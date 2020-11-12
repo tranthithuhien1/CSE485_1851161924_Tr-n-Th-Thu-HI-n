73d8e0228a9a5b287a732f943fb6694447928568
tranthithuhien1 <67354218+tranthithuhien1@users.noreply.github.com>
2020-11-12 23:05:13 +0700

    jsk

--- final_test/edit.php
+++ final_test/edit.php
@@ -1,10 +1,8 @@
-<<?php   
-    
-    require_once('app/database/connect.php');
-    $id=$_GET['id'];
-    $sql="SELECT * FROM fem where id=$id";
-    $result = mysqli_query($dbcon, $sql);
-    $row=mysqli_fetch_array($result);
+<?php
+require('app/database/config.php');
+$userid=$_GET['userid'];
+$query=mysqli_query($conn,"SELECT * FROM fem where userid='$userid'");
+$row=mysqli_fetch_assoc($query);
 ?>
 <!doctype html>
 <html lang="en">
@@ -15,60 +13,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
+    <link rel="shortcut icon" href="../img/favicon.ico">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-    <div class="col-sm-8">
-       <h2 class="h2 text-center"> Edit Register</h2>
-        <form action='process-edit.php?id=<?php echo $row['id']?>' method="POST" onsubmit="return checked();"name="regform" id="regform">
-            <div class="form-group row">
-                <label for="first_name" class="col-sm-4 col-form-label">User name:</label>
-            <div class="col-sm-8">
-            <input type="text" class="form-control" id="first_name" name="username" 
-	            placeholder="username" maxlength="30" required
-	            value="<?php  echo $row['username']; ?>" >
-    </div>
-  </div>
-    <div class="form-group row">
-        <label for="last_name" class="col-sm-4 col-form-label">Display name:</label>
-        <div class="col-sm-8">
-        <input type="text" class="form-control" id="last_name" name="displayname" 
-	      placeholder="displayname" maxlength="40" required
-	      value="<?php echo $row['displayname']; ?>">
-        </div>
-    </div>
-<div class="form-group row">
-    <label for="email" class="col-sm-4 col-form-label">E-mail:</label>
-    <div class="col-sm-8">
-      <input type="email" class="form-control" id="email" name="email" 
-	  placeholder="E-mail" maxlength="60" required
-	  value="<?php  echo $row['email']; ?>">
-    </div>
-  </div>
-<div class="form-group row">
-    <label for="password1" class="col-sm-4 col-form-label">Password:</label>
-    <div class="col-sm-8">
-      <input type="password" class="form-control" id="password1" name="password1" 
-	  placeholder="Password" minlength="8" maxlength="12" required
-	  value="">
-	  <span id='message'>Between 8 and 12 characters.</span>
-    </div>
-  </div>
-<div class="form-group row">
-    <label for="password2" class="col-sm-4 col-form-label">Confirm Password:</label>
-    <div class="col-sm-8">
-      <input type="password" class="form-control" id="password2" name="password2" 
-	  placeholder="Confirm Password" minlength="8" maxlength="12" required
-	  value="">
-    </div>
-  </div>
-    <div class="form-group row">
-        <div class="col-sm-12">
-	        <input id="submit" class="btn btn-primary" type="submit" name="submit" value="Edit">
-        </div>
-	    </div>
-	</form>
+  <div class="text-center">
+        <h3>Sửa dữ liệu</h3>
+        <form action="" method="post">
+             <div class="form-group">
+                <label for="">userid</label>
+                <input type="number"  id="userid" name="userid"value="<?php echo $row['userid'] ; ?>">
+            </div> 
+            <div class="form-group">
+                <label for="">Username</label>
+                <input type="text" id="username" name="username"value="<?php echo $row['username'] ; ?>">
 </div>
+            <div class="form-group">
+                <label for="">Password</label>
+                <input type="text"  id="password" name="password"value="<?php echo $row['password'] ; ?>">
+            </div>
+            <div class="form-group">
+                <label for="">Displayname</label>
+                <input type="text" id="displayname" name="displayname"value="<?php echo $row['displayname'] ; ?>">
+            </div>
+            <div class="form-group">
+                <label for="">Email</label>
+                <input type="text" id="email" name="email" value="<?php echo $row['email'] ; ?>">
+            </div>
+            <div class="form-group">
+                <label for="">registration_date</label>
+                <input type="text" id="registration_date" name="registration_date" value="<?php echo $row['registration_date'] ; ?>">
+            </div>
+            <button name='update'>Save</button>
+            <a href="user.php"  class="btn btn-primary">Back</a>
+            <?php
+                if (isset($_POST['update'])){
+                $userid=$_GET['userid'];
+                $username=$_POST['username'];
+               
+                $password=$_POST['password'];
+                $displayname=$_POST['displayname'];
+                $email=$_POST['email'];
+                $registration_date=$_POST['registration_date'];
+                $conn = new mysqli("localhost", "root", "", "blog");
+                
+                if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+                }
+                
+                $sql = "UPDATE fem SET usename='$username', password='$password',displayname='$displayname', email='$eamil', registration_date='$registration_date' WHERE userid='$userid'";
+                
+                if ($conn->query($sql) === TRUE) {
+                 ?> <script> alert('Đã Cập Nhật Thành Công'); </script><?php
+                } else {
+                echo "Error updating record: " . $conn->error;
+                }
+                
+                $conn->close();
+                }
+                ?>
+           <!--  <button>quaylai</button> -->
+        </form>
+        
+      </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
